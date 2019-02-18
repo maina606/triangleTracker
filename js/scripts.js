29b4a7d3e43e1a49424cf6c8d2b0e61a23d0e3d6
maina606 <cmmaina@gmail.com>
2019-02-18 09:46:07 +0300

    Edit README:MD file

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,9 @@
-/*User Logic - user interaction with the Front-end/
+/*User Logic - user interaction with the Front-end part/
 
 
 
 
-
-/*Business Logic - Functionality */
-
-
-/*
-function myFunction() {document.getElementById("frm1").submit();}  */
-
-
-
-/*function myFunction() {
-  var x = document.getElementById("frm1");
-  var text = "";
-  var i;
-  for (i = 0; i < x.length ;i++) {
-    text += x.elements[i].value + "<br>";
-  }
-  document.getElementById("demo").innerHTML = text;
-}
-
-/*
-var triangles = function(a, b, c){
-
-    if (a === b && a === c && b === c){
-        prompt("This is an equilateral triangle, since all the sides are equal.");
-    } else if (a === b || b===c ||a === c) {
-        prompt("This is an isosceles triangle, since two of the sides are equal.");
-    } else if (a != b && b != c && a != c) {
-        prompt("This must be a scalene triangle, since none of the sides are equal.");
-    } else {
-         prompt("Not a triangle!");
-    }
-  return (triangles);
-}
-*/
-
-
-/* */
-
+/* ----------------------Business Logic - Functionality------------------------------*/
 var triangles = function (a, b, c){
   if (a === b && a === c && b === c){
     return "This is an equilateral triangle, since all the sides are equal.";
@@ -68,6 +31,7 @@ var result = triangles(a, b, c);
 
 alert(result);
 
+
 /*
 function myFunction() {
   var side1 = one;// get input from first input box
@@ -75,24 +39,22 @@ function myFunction() {
   var side3 = three;// get input from first input box
 alert(triangles(side1, side2, side3));
 }
+*/
 
 
+/* ---------------------- Trials ------------------------------*/
+/*
+function myFunction(form) {
+document.getElementById("frm1").submit();}
 
-
-
+*/
 
 /*
-var numEven = function (num1, num2){
-	return num1 / num2;
-};
-var num1 = parseInt(prompt("Enter a number:"));
-var num2 = parseInt(prompt("Enter another number:"));
-var result = numEven(num1, num2);
-alert(result);
-
-if(result % 2 === 0){ //if the reminder is 0 after dividing 2 numbers, then even
-	alert("This is an even number");
-} else {
-	alert("This is an odd number!");
-}
+function myFunction(form) {
+  var x = document.getElementById("frm1");
+  var text = "";
+  var i;
+  for (i = 0; i < x.length ;i++) {
+    text += x.elements[i].value + "<br>";
+  }
 */
