5b15ea2ab5f94fe7927c16077bf62f5952d19954
maina606 <cmmaina@gmail.com>
2019-02-22 09:42:05 +0300

    modified nested if and now working

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,49 @@
-/*User Logic - user interaction with the Front-end part/
-
-
-
 
 /* ----------------------Business Logic - Functionality------------------------------*/
 
 
-/* ---------------------- Form------------------------------*/
-/*
-function myFunction() {
-  var side1 = one;// get input from first input box
-  var side2 = two;// get input from first input box
-  var side3 = three;// get input from first input box
-alert(triangles(side1, side2, side3));
-}
-
-function myFunction(form) {
-document.getElementById("frm1").submit();}
-*/
-
-/*
-function myFunction(form) {
-  var x = document.getElementById("frm1");
-  var digits = "";
-  var i;
-  for (i = 0; i < x.length ;i++) {
-    digits += x.elements[i].value + "<br>";
-  }
-*/
 
 
-/*
 var triangles = function (a, b, c){
-  if (a + b > c || b + c > a || a + c > b){
-    if (a === b && a === c && b === c){
-      if (a === b || b===c ||a === c) {
-        if (a != b && b != c && a != c) {
-          return "This must be a scalene triangle, since none of the sides are equal.";
-        } else {
-            return "This is an isosceles triangle, since two of the sides are equal.";
-        } else {
-            return "This is an equilateral triangle, since all the sides are equal.";
-        } else {
-          return "Triangle CANNOT be formed using these values!";
-        }
+  //debugger
+  if (a + b > c && b + c > a && a + c > b){ // if the 3 sides make a triangle
+    if (a === b && a === c && b === c){ // if all the sides are equal
+      return "This is an equilateral triangle, since all the sides are equal.";
+    }
+    else if (a === b || b===c ||a === c) { // if two sides are equal
+      return "This is an isosceles triangle, since two of the sides are equal.";
+    }
+    else if (a != b && b != c && a != c) { // if none of the sides are equal
+      return "This must be a scalene triangle, since none of the sides are equal.";
+    }
+  } else {// if the 3 sides dont make a triangle
+    return "Not a triangle";
   }
 }
 
-var sides = parseInt("Enter 3 numbers: " + a, b, c);
+/* ---------------User Logic - user interaction with the Front-end part ----------------*/
+/* ---------------------- Form------------------------------*/
+function myFunction() {
 
-var a = parseInt(prompt("Enter a number:"));
-var b = parseInt(prompt("Enter another number:"));
-var c = parseInt(prompt("Enter yet another number:"));
-var result = triangles(a, b, c);
+  var a = parseInt(document.getElementById("one").value);
+  var b = parseInt(document.getElementById("two").value);
+  var c = parseInt(document.getElementById("three").value);
+  // console.log(a);
+  // console.log(b);
+  // console.log(c);
+  var result = triangles(a, b, c);
 
-alert(result);
+  alert(result)
+}
 
-*/
+
+
+
+
 
 
 /* ---------------------------------------------------- */
-
+/*
 
 var triangles = function (a, b, c){
   if (a === b && a === c && b === c){
@@ -88,3 +71,5 @@ var c = parseInt(prompt("Enter yet another number:"));
 var result = triangles(a, b, c);
 
 alert(result);
+
+*/
